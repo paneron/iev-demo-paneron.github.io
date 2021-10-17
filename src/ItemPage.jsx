9385d01957b192137d8f45dd87551ac6da289816
Alexande B <abobrikovich@gmail.com>
2021-10-17 09:06:18 +0200

    paneron/dataset-build-action#1 migrate to paneron/dataset-build-action

--- src/ItemPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import DefaultItemPage from '@riboseinc/react-static-plugin-paneron-registry/DefaultRegisterItemPage';
-import { itemClassConfiguration } from '@riboseinc/paneron-extension-glossarist/registryConfig';
-
-
-export default () => (
-  <DefaultItemPage itemClassConfiguration={itemClassConfiguration} />
-)
